a879ae3835d58aac5c2e1865a833bad197546f50
Aimee Teo Broman <abroman@biostat.wisc.edu>
2014-06-10 10:56:33 -0500

    Adding wrappers for bp, ILP and sequentialRM

---
+++ R/getPi.R
@@ -0,0 +1,10 @@
+
+getPi <- function(I,y,alpha,gamma){
+    require(mgsa)
+    Imgsa <- lapply(1:ncol(I), function(j)
+                    as.character(which(I[,j]==1)))
+    Imgsa <- new("MgsaSets",sets=Imgsa)
+    fit <- mgsa(as.character(which(y==1)),
+                Imgsa,alpha=alpha,beta=1-gamma,restarts=20)
+    fit@pPost$value[which.max(fit@pPost$estimate)]
+}
