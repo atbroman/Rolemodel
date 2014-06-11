33efaf01c15945f859af58720f339569eaf32cbc
Aimee Teo Broman <abroman@biostat.wisc.edu>
2014-06-11 08:26:35 -0500

    renamed again

--- R/bpTable.R
+++ R/bpTable.R
@@ -1,6 +1,6 @@
 ## bpout = output from bp() function
 
-bpsub <- function(bpout,pr=0.5){
+bpTable <- function(bpout,pr=0.5){
     bpord <- bpout[order(bpout$ActiveProbability,decreasing=TRUE),]
     bpord[bpord$ActiveProbability>pr,1:2]
 }
